7002cc18bf19ed02f5cbeb9da09eb80def8968ff
Zeeshan <zeeshan.mimanshatech@gmail.com>
2022-10-07 16:14:23 +0530

    basic use of ts file

--- practice_project/src/app/app.component.ts
+++ practice_project/src/app/app.component.ts
@@ -1,10 +1,13 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: `<apple></apple>`,
+  template: `<h2> APPLE </h2>`,
+  styles: [`h2{
+    color: red;
+  }`]
 })
-export class AppComponent {
-  title = 'practice_project';
-}
+
+ export class AppComponent{
+
+ }
