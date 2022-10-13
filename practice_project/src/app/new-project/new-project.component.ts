1967f21dafe7f4929ad7f73eff8f160ea215b46b
Zeeshan <zeeshan.mimanshatech@gmail.com>
2022-10-13 10:32:09 +0530

    to inject the typescript interpolation we use double curly braces like {{}}

--- practice_project/src/app/new-project/new-project.component.ts
+++ practice_project/src/app/new-project/new-project.component.ts
@@ -1,15 +1,18 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-new-project',
   templateUrl: './new-project.component.html',
   styleUrls: ['./new-project.component.css']
 })
-export class NewProjectComponent implements OnInit {
+export class NewProjectComponent  {
+  name:string = "Zeeshan";
+  age:number = 24;
+  salary:number = 100;
+  
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  // creating a method
+  getname(){
+    return 'I am batman'
+  };
 }
