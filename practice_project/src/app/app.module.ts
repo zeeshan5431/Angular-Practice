0ffd643e24db8bc5b8763ab04973c92ec4a7af53
Zeeshan <zeeshan.mimanshatech@gmail.com>
2022-10-13 10:03:13 +0530

    i use ng generate component <component-name> to generate a component

--- practice_project/src/app/app.module.ts
+++ practice_project/src/app/app.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
-import { ProfileComponent } from './profile/profile.component';
+import { NewProjectComponent } from './new-project/new-project.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProfileComponent,
+    NewProjectComponent
   ],
   imports: [
     BrowserModule
